aaa9124eabff7d725e664ffa9ca61de119976bff
Jose A González <jagascripts@gmail.com>
2022-04-10 01:07:50 +0200

    Generado el proyecto y ejercicio 1 por implementar

---
+++ ud6/src/ejercicio1/ejercicio1.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+/**
+ * 
+ * @author Jose Antonio Gonz�lez Alc�ntara
+ * 
+ * Clase de Java de la TA06-Metodos y Arrays Ejercicio 1
+ *
+ */
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

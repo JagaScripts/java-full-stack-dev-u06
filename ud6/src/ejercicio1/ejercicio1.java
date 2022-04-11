027ba847baada0bd0539cf7b2df9adc911832595
Jose A González <jagascripts@gmail.com>
2022-04-11 09:52:53 +0200

    Ejercico 1 cambios que había echo desde ubuntu y no había confirmado ni subido

--- ud6/src/ejercicio1/ejercicio1.java
+++ ud6/src/ejercicio1/ejercicio1.java
@@ -1,7 +1,13 @@
 package ejercicio1;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+
 /**
  * 
- * @author Jose Antonio Gonz�lez Alc�ntara
+ * @author Jose Antonio González Alcántara
  * 
  * Clase de Java de la TA06-Metodos y Arrays Ejercicio 1
  *
@@ -9,7 +15,165 @@ package ejercicio1;
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Crea una aplicación que nos calcule
+		  el área de un circulo, cuadrado o triangulo.
+		  Pediremos que figura queremos calcular su área
+		  y según lo introduccido pedirá los valores 
+		  necesarios para calcular el área.
+		  Crear un método por cad figura para
+		  calcular cada área, este devolverá un número real.
+		  Muestra el resultado por pantalla*/
+		
+		pedirFigura();
+		
+	}
+	
+	public static String validarEntrada(String valores) {
+		
+		String entradaTeclado;
+		boolean esNumerico;
+		
+		do {
+
+			entradaTeclado = JOptionPane.showInputDialog(
+					"Introduce " + valores + ", el valor tienen que ser númerico," + " el separador decimal es el . (punto)");
+			
+
+			// Si cancelamos el pane
+			if (entradaTeclado != null) {
+
+				// Como aún no hemos visto manejo de excepciones
+				// Compruebo mediante el metodo matches del tipo String si lo introducido es un
+				// número
+				esNumerico = entradaTeclado.matches("[+-]?\\d*(\\.\\d+)?");
+
+				if (esNumerico) {
+
+					return entradaTeclado;
+
+				} else {
+
+					JOptionPane.showMessageDialog(null, "No has introducido un número valido");
+
+				}
+
+			} else {
+				
+				JOptionPane.showMessageDialog(null, "La aplicación se cerrara");
+				esNumerico = false;
+			}
+
+		} while (!esNumerico && entradaTeclado != null);
+		
+		return entradaTeclado;
+		
+	}
+	
+	public static Double calcularAreaCirculo() {
+
+		// Uso la constante PI de Math y la asigno a mi constante PI
+		final double PI = Math.PI;
+
+		// Declaramos la variable que almacena el texto introducido por el usuario y el
+		// radio
+		String entradaTeclado;
+		double radioCirculo;
+		double areaCirculo;
+		boolean esNumerico;
+
+		do {
+
+			entradaTeclado = JOptionPane.showInputDialog(
+					"Introduce el radio, el valor tienen que ser númerico," + " el separador decimal es el . (punto)");
+			
+			areaCirculo = 0.0;
+
+			// Si cancelamos el pane
+			if (entradaTeclado != null) {
+
+				// Como aún no hemos visto manejo de excepciones
+				// Compruebo mediante el metodo matches del tipo String si lo introducido es un
+				// número
+				esNumerico = entradaTeclado.matches("[+-]?\\d*(\\.\\d+)?");
+
+				if (esNumerico) {
+
+					// Parseamos a double el texto para que sea un double
+					radioCirculo = Double.parseDouble(entradaTeclado);
+
+					// Calculamos el area del circulo
+					areaCirculo = PI * Math.pow(radioCirculo, 2);
+					
+					return areaCirculo;
+
+
+				} else {
+
+					JOptionPane.showMessageDialog(null, "No has introducido un número valido");
+
+				}
+
+			} else {
+
+				esNumerico = false;
+			}
+
+		} while (!esNumerico && entradaTeclado != null);
+		
+		return areaCirculo;
+	}
+	
+	public static void pedirFigura() {
+
+		String entradaTeclado;
+		Scanner scanner;
+		boolean esOpcionValida;
+		char opcion;
+
+		scanner = new Scanner(System.in);
+			
+
+		do {
+
+			System.out.println("Menú de opciones para calcular areas");
+			System.out.println("1. Circulo");
+			System.out.println("2. Triangulo");
+			System.out.println("3. Cuadrado");
+			System.out.println("Elige el número de la figura que quieres calcular su area");
+			entradaTeclado = scanner.nextLine();
+			esOpcionValida = (entradaTeclado.length() == 1);
+
+			if (esOpcionValida) {
+
+				opcion = (char) entradaTeclado.charAt(0);
+
+				switch (opcion) {
+				case '1':
+					// Mostramos resultado en pantalla
+					JOptionPane.showMessageDialog(null,"El area del circulo es " + calcularAreaCirculo());
+					break;
+				case '2':
+					JOptionPane.showMessageDialog(null,"El area del circulo es " + calcularAreaCirculo());
+					break;
+				case '3':
+					JOptionPane.showMessageDialog(null,"El area del circulo es " + calcularAreaCirculo());
+					break;
+				default:
+					System.out.println("Elige una opción válida");
+					esOpcionValida = false;
+					break;
+				}
+
+			} else {
+				
+				System.out.println("Porfavor introduce un solo caracter");
+				esOpcionValida = false;
+				
+			}
+
+		} while (!esOpcionValida);
+
+		scanner.close();
 
 	}
 
